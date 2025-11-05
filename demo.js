253255866abd739253d4dce680bdd2a8303b7667
muhdfrhan <paanirate@gmail.com>
2025-11-05 22:04:07 +0800

    Add demo.js and test.js with basic greeting functionality

---
+++ demo.js
@@ -0,0 +1,13 @@
+// Using console.log for debugging
+console.log("Hello from console!");
+
+// Alert box display
+alert("Hello from alert!");
+
+// Writing to document (HTML)
+document.write("Hello from document!");
+
+// Creating and manipulating DOM elements
+const div = document.createElement('div');
+div.textContent = "Hello from DOM!";
+document.body.appendChild(div);
---
+++ test.js
@@ -0,0 +1,7 @@
+// Simple function to display a message
+function displayMessage(message) {
+    console.log(message);
+}
+
+// Sample usage
+displayMessage("Hello, World!");
