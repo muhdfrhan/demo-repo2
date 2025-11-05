5746f55139acb6e8469bd7705c8850a42fa74481
muhdfrhan <paanirate@gmail.com>
2025-11-05 23:10:55 +0800

    Add display.js and showText.js with greeting messages

---
+++ display.js
@@ -0,0 +1,7 @@
+// filename: displayString.js
+
+// Define a string
+const message = "Hello, Farhan! Welcome to JavaScript.";
+
+// Display the string in the console
+console.log(message);
---
+++ showText.js
@@ -0,0 +1,7 @@
+// filename: showText.js
+
+// Define another string
+const text = "JavaScript makes coding fun!";
+
+// Display the string in the console
+console.log(text);
