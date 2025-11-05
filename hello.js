33e181fe3ddbe556a6ff65ce53f9f376fbc9fa3b
muhdfrhan <paanirate@gmail.com>
2025-11-05 22:59:17 +0800

    Update greeting message in hello.js for clarity

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 // hello.js - simple greeting
 const name = process.argv.slice(2).join(' ') || 'World';
-console.log(`Hello, ${name}!`);
+console.log(`Hello my name is, ${name}!`);
